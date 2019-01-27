eb790251434d9078219e3e6b2d1e083dfe371461
Ali Kaafarani <ali@kvikshaug.no>
2019-01-27 17:35:10 +0100

    Add admin section

--- benssalong.py
+++ benssalong.py
@@ -1,32 +1,74 @@
-from flask import Flask, render_template
+import os
 
-from db import data
+from flask import Flask, redirect, render_template, request, session, url_for
+
+import db
 
 
 app = Flask(__name__)
 app.debug = True
+app.secret_key = os.urandom(32)
+app.config['PASSWORD'] = os.environ['PASSWORD']
 
 
 @app.route("/")
 def home():
-    return render_template("home.html", page="home", data=data)
+    return render_template("home.html", page="home", data=db.data)
 
 
 @app.route("/varetilbud")
 def varetilbud():
-    return render_template("varetilbud.html", page="varetilbud", data=data)
+    return render_template("varetilbud.html", page="varetilbud", data=db.data)
 
 
 @app.route("/galleri")
 def galleri():
-    return render_template("galleri.html", page="galleri", data=data)
+    return render_template("galleri.html", page="galleri", data=db.data)
 
 
 @app.route("/omoss")
 def omoss():
-    return render_template("omoss.html", page="omoss", data=data)
+    return render_template("omoss.html", page="omoss", data=db.data)
 
 
 @app.route("/kontakt")
 def kontakt():
-    return render_template("kontakt.html", page="kontakt", data=data)
+    return render_template("kontakt.html", page="kontakt", data=db.data)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if 'authenticated' in session:
+        return redirect(url_for('admin'))
+    failed_login = False
+    if request.method == 'POST':
+        if request.form.get('password', '') == app.config['PASSWORD']:
+            session['authenticated'] = True
+            return redirect(url_for('admin'))
+        else:
+            failed_login = True
+    return render_template("login.html", page="login", data=db.data, failed_login=failed_login)
+
+
+@app.route("/admin", methods=["GET", "POST"])
+def admin():
+    if 'authenticated' not in session:
+        return redirect(url_for('login'))
+    
+    if request.method == "GET":
+        return render_template("admin.html", page="admin", data=db.data_unescaped())
+    else:
+        db.save(
+            åpningstider=request.form['åpningstider'],
+            priser=request.form['priser'],
+            timebestilling=request.form['timebestilling'],
+            nyheter=request.form['nyheter'],
+            varetilbud=request.form['varetilbud'],
+        )
+        return render_template("admin.html", page="admin", data=db.data_unescaped(), saved=True)
+
+
+@app.route("/loggut")
+def logout():
+    session.pop('authenticated', None)
+    return redirect(url_for('home'))
--- db.py
+++ db.py
@@ -1,5 +1,26 @@
+import html
 import json
 
 
 with open("database.json") as file_:
     data = json.load(file_)
+
+
+def data_unescaped():
+    data_unescaped = data.copy()
+    data_unescaped['åpningstider'] = html.unescape(data_unescaped['åpningstider'])
+    data_unescaped['priser'] = html.unescape(data_unescaped['priser'])
+    data_unescaped['timebestilling'] = html.unescape(data_unescaped['timebestilling'])
+    data_unescaped['nyheter'] = html.unescape(data_unescaped['nyheter'])
+    data_unescaped['varetilbud'] = html.unescape(data_unescaped['varetilbud'])
+    return data_unescaped
+
+
+def save(åpningstider, priser, timebestilling, nyheter, varetilbud):
+    data['åpningstider'] = html.escape(åpningstider)
+    data['priser'] = html.escape(priser)
+    data['timebestilling'] = html.escape(timebestilling)
+    data['nyheter'] = html.escape(nyheter)
+    data['varetilbud'] = html.escape(varetilbud)
+    with open("database.json", "w") as file_:
+        json.dump(data, file_)
