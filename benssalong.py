5992347970858f772789696ebae7851f72f3f9fc
Ali Kaafarani <ali@kvikshaug.no>
2019-01-27 16:40:49 +0100

    Database

--- benssalong.py
+++ benssalong.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 
+from db import data
+
 
 app = Flask(__name__)
 app.debug = True
@@ -7,24 +9,24 @@ app.debug = True
 
 @app.route("/")
 def home():
-    return render_template("home.html", page="home")
+    return render_template("home.html", page="home", data=data)
 
 
 @app.route("/varetilbud")
 def varetilbud():
-    return render_template("varetilbud.html", page="varetilbud")
+    return render_template("varetilbud.html", page="varetilbud", data=data)
 
 
 @app.route("/galleri")
 def galleri():
-    return render_template("galleri.html", page="galleri")
+    return render_template("galleri.html", page="galleri", data=data)
 
 
 @app.route("/omoss")
 def omoss():
-    return render_template("omoss.html", page="omoss")
+    return render_template("omoss.html", page="omoss", data=data)
 
 
 @app.route("/kontakt")
 def kontakt():
-    return render_template("kontakt.html", page="kontakt")
+    return render_template("kontakt.html", page="kontakt", data=data)
---
+++ db.py
@@ -0,0 +1,5 @@
+import json
+
+
+with open("database.json") as file_:
+    data = json.load(file_)
